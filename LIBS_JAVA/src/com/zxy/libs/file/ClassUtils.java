59d373efc12eb4326544905f005291e605d4fd9d
Asmot <renyanrenren@gmail.com>
2015-09-01 20:27:56 +0800

    获取包名下的所有类 写demo的时候可以用，自动添加Activity

---
+++ LIBS_JAVA/src/com/zxy/libs/file/ClassUtils.java
@@ -0,0 +1,184 @@
+package com.zxy.libs.file;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class ClassUtils {
+
+	/**
+	 * 获取某一包名下得所有类
+	 * 可以是编译好的class文件也可以是jar包
+	 * 
+	 * @param pkgName
+	 */
+	public static List<Class<?>> getClasses(String pkgName) {
+
+		List<Class<?>> classes = new ArrayList<>();
+
+		// 将包名中的'.'换成'/'
+		pkgName = pkgName.replace(".", "/");
+
+		// 是否循环迭代
+		boolean recursive = true;
+
+		Enumeration<URL> dirs;
+
+		try {
+			dirs = Thread.currentThread().getContextClassLoader()
+					.getResources(pkgName);
+
+			while (dirs.hasMoreElements()) {
+				URL url = dirs.nextElement();
+
+				// 有jar和file两种 应该就是本地文件和引包
+				String protocol = url.getProtocol();
+				if (protocol.equals("file")) {
+					// 获取物理地址
+					String filePath = URLDecoder.decode(url.getFile(), "utf-8");
+
+					// 然后从文件中读取类
+					fetchAllClassesInFile(pkgName, filePath, true, classes);
+				} else if (protocol.equals("jar")) {
+					//如果是jar包文件 
+                    //定义一个JarFile
+                    JarFile jar;
+                    try {
+                        //获取jar
+                        jar = ((JarURLConnection) url.openConnection()).getJarFile();
+                        //从此jar包 得到一个枚举类
+                        Enumeration<JarEntry> entries = jar.entries();
+                        //同样的进行循环迭代
+                        while (entries.hasMoreElements()) {
+                            //获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
+                            JarEntry entry = entries.nextElement();
+                            String name = entry.getName();
+                            //如果是以/开头的
+                            if (name.charAt(0) == '/') {
+                                //获取后面的字符串
+                                name = name.substring(1);
+                            }
+                            //如果前半部分和定义的包名相同
+                            if (name.startsWith(pkgName)) {
+                                int idx = name.lastIndexOf('/');
+                                //如果以"/"结尾 是一个包
+                                if (idx != -1) {
+                                    //获取包名 把"/"替换成"."
+                                	pkgName = name.substring(0, idx).replace('/', '.');
+                                }
+                                //如果可以迭代下去 并且是一个包
+                                if ((idx != -1) || recursive){
+                                    //如果是一个.class文件 而且不是目录
+                                    if (name.endsWith(".class") && !entry.isDirectory()) {
+                                        //去掉后面的".class" 获取真正的类名
+                                        String className = name.substring(pkgName.length() + 1, name.length() - 6);
+                                        try {
+                                            //添加到classes
+                                            classes.add(Class.forName(pkgName + '.' + className));
+                                        } catch (ClassNotFoundException e) {
+                                            e.printStackTrace();
+                                        }
+                                      }
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } 
+
+				}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return classes;
+	}
+
+	/**
+	 * 以文件的形式来获取包下的所有Class
+	 * 
+	 * @param packageName 包名
+	 * @param packagePath 文件路径
+	 * @param recursive true表示递归查询
+	 * @param classes 存放class的list
+	 */
+	public static void fetchAllClassesInFile(String packageName,
+			String packagePath, final boolean recursive, List<Class<?>> classes) {
+		// 获取此包的目录 建立一个File
+		File dir = new File(packagePath);
+		// 如果不存在或者 也不是目录就直接返回
+		if (!dir.exists() || !dir.isDirectory()) {
+			return;
+		}
+		// 如果存在 就获取包下的所有文件 包括目录
+		File[] dirfiles = dir.listFiles(new FileFilter() {
+			// 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
+			public boolean accept(File file) {
+				return (recursive && file.isDirectory())
+						|| (file.getName().endsWith(".class"));
+			}
+		});
+		// 循环所有文件
+		for (File file : dirfiles) {
+			// 如果是目录 则继续扫描
+			if (file.isDirectory()) {
+				fetchAllClassesInFile(
+						packageName + "." + file.getName(),
+						file.getAbsolutePath(), recursive, classes);
+			} else {
+				// 如果是java类文件 去掉后面的.class 只留下类名
+				String className = file.getName().substring(0,
+						file.getName().length() - 6);
+				try {
+					// 添加到集合中去
+					//Class.forName的时候形式为 xx.xx.xx  不是 xx/xx/xx
+					String tmpPkgName = packageName.replace("/", ".");
+					classes.add(Class.forName(tmpPkgName + '.' + className));
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+     * 取得某个接口下所有实现这个接口的类
+     * */
+    public static List<Class> getAllClassByInterface(Class c) {
+            List<Class>  returnClassList = null;
+            
+            if(c.isInterface()) {
+                // 获取当前的包名
+                String packageName = c.getPackage().getName();
+                // 获取当前包下以及子包下所以的类
+                List<Class<?>> allClass = getClasses(packageName);
+                if(allClass != null) {
+                    returnClassList = new ArrayList<Class>();
+                    for(Class classes : allClass) {
+                        // 判断是否是同一个接口
+                        if(c.isAssignableFrom(classes)) {
+                            // 本身不加入进去
+                            if(!c.equals(classes)) {
+                                returnClassList.add(classes);        
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return returnClassList;
+        }
+
+
+}
--- LIBS_JAVA/src/com/zxy/libs/file/Test.java
+++ LIBS_JAVA/src/com/zxy/libs/file/Test.java
@@ -1,10 +1,19 @@
 package com.zxy.libs.file;
 
+import java.util.List;
+
+
 public class Test {
 
 	public static void main(String[] args) {
 //		FileUtils.readFileByLine();
 //		FileUtils.readFileByByte();
+		
+		List<Class<?>> classes = ClassUtils.getClasses("com.zxy.libs.file");
+		
+		for(Class<?> c : classes)  {
+			System.out.println(c.getName());
+		}
 	}
 
 }
